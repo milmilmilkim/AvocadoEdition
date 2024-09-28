00cae900695b66e29cd97f62ec1ef55aaea5d987
milmilkim <soundkim27@gmail.com>
2024-09-28 18:48:26 +0900

    inc 주석

--- AvocadoEdition/skin/board/mmb_samnamwol/write_comment_update.skin.php
+++ AvocadoEdition/skin/board/mmb_samnamwol/write_comment_update.skin.php
@@ -1,35 +1,36 @@
 <?php
-if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
- 
+if (!defined("_GNUBOARD_"))
+	exit; // 개별 페이지 접근 불가
+
 $customer_sql = "";
 $temp_wr_id = $comment_id;
-$wr_num = $wr['wr_num']; 
+$wr_num = $wr['wr_num'];
 
-include_once($board_skin_path.'/write_update.inc.php');  
+// include_once($board_skin_path.'/write_update.inc.php');  
 
-if($bf_file_del){
-	$file=sql_fetch("select * from {$g5['board_file_table']} where bo_table='{$bo_table}' and wr_id='{$comment_id}' and bf_no='0'");
+if ($bf_file_del) {
+	$file = sql_fetch("select * from {$g5['board_file_table']} where bo_table='{$bo_table}' and wr_id='{$comment_id}' and bf_no='0'");
 	sql_query("delete from {$g5['board_file_table']} where bo_table='{$bo_table}' and wr_id='{$comment_id}' and bf_no='0'");
 	sql_query("update {$write_table} set wr_file='0', wr_type='' where wr_id='{$comment_id}'");
 }
 
- if($_FILES['bf_file']['name']){ 
-	$files=upload_c_file('bf_file',$bo_table); 
-	$file=sql_fetch("select * from {$g5['board_file_table']} where bo_table='{$bo_table}' and wr_id='{$comment_id}' and bf_no='0'");
-	$sql_common="bf_source='{$files['source']}',
+if ($_FILES['bf_file']['name']) {
+	$files = upload_c_file('bf_file', $bo_table);
+	$file = sql_fetch("select * from {$g5['board_file_table']} where bo_table='{$bo_table}' and wr_id='{$comment_id}' and bf_no='0'");
+	$sql_common = "bf_source='{$files['source']}',
 				bf_file='{$files['name']}',
 				bf_filesize='{$files['size']}',
 				bf_width = '{$files['img'][0]}',
 				bf_height = '{$files['img'][1]}',
 				bf_type = '{$files['img'][2]}',
 				bf_content = '{$bf_content}',
-				bf_datetime = '".G5_TIME_YMDHIS."' ";
-	if($file['wr_id']){
+				bf_datetime = '" . G5_TIME_YMDHIS . "' ";
+	if ($file['wr_id']) {
 		sql_query("update {$g5['board_file_table']}
 			set {$sql_common}
 				where bo_table = '{$bo_table}' and wr_id='{$comment_id}' and bf_no = '0'
 		");
-	}else{
+	} else {
 		sql_query("insert into {$g5['board_file_table']}
 			set {$sql_common},
 				wr_id = '{$comment_id}' ,
@@ -39,9 +40,9 @@ if($bf_file_del){
 
 	}
 	$customer_sql .= ",wr_type='UPLOAD', wr_file='1', wr_width='{$files['img'][0]}', wr_height='{$files['img'][1]}'";
- }
+}
 
-if($w != 'cu') {  
+if ($w != 'cu') {
 	$sql = " update {$write_table}
 				set wr_subject = '{$wr_subject}'
 				{$customer_sql}
@@ -57,5 +58,5 @@ if($w != 'cu') {
 }
 
 
-goto_url('./board.php?bo_table='.$bo_table.'&amp;'.$qstr.'&amp;#c_'.$comment_id);
+goto_url('./board.php?bo_table=' . $bo_table . '&amp;' . $qstr . '&amp;#c_' . $comment_id);
 ?>
